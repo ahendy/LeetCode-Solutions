0fdc0bc51fbbf97b50087232181e5b94593daa14
ahendy0 <ahendy@hotmail.ca>
2016-08-20 12:52:30 -0700

    RemoveSortedDuplicates

---
+++ LeetCode/RemoveSortedDuplicates.py
@@ -0,0 +1,7 @@
+from collections import OrderedDict
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+
+        nums[:] =  OrderedDict.fromkeys(nums).keys()
+        return len(nums)
