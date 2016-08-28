1c33e0b3b8517efd213aa1f71b25446caab62a76
Austin Hendy <ahendy@hotmail.ca>
2016-08-28 00:21:10 -0700

    Delete Node form list

---
+++ LeetCode/Add-digits.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num<9: return num
+        return num - 9*((num-1)//9)
